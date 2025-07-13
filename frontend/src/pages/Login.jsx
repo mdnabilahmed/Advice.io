7fa656ebc76e16758be231a240399939c6ca4c02
mdnabilahmed <mdnabilahmed706@gmail.com>
2025-07-14 03:06:32 +0530

    send

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div>Login</div>
+      <div>start</div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
