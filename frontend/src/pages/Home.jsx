ee57114274f75e3d2f28631bcb4c0e8d75c4b1b2
mdnabilahmed <mdnabilahmed706@gmail.com>
2025-07-14 08:03:32 +0530

    changes 4 login page done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,17 +3,29 @@ import Navbar from "/Advice/frontend/src/components/Navbar";
 
 const App = () => {
   return (
-    <div className="bg-gradient-to-br from-[#f0f4ff] via-[#fce4ec] to-[#e0f7fa] min-h-screen no-scrollbar overflow-y-scroll">
+    <div className="bg-gradient-to-br from-[#f0f4ff] via-[#fcece4] to-[#e0f7fa] min-h-screen">
       <Navbar />
-
-      <div className="h-screen flex flex-col justify-center items-center text-xl font-bold text-center mb-30">
-        Real people. Real-time advice. Or just let AI handle it <br /> — your
-        call. llakj
+      <div className="pt-45 flex flex-col justify-center items-center">
+        <div className=" text-5xl font-bold text-center">
+          Real people. Real-time advice. Or just let AI handle it <br />{" "}
+          <span className="text-violet-600">— your call.</span>
+        </div>
+        <p className="text-center pt-4">
+          Get the advice you need — fast. Whether it comes from a person or AI,
+          what matters is that it makes sense, saves time, <br /> and helps you
+          move forward confidently.
+        </p>
+        <div className="pt-4 text-white">
+          <button className=" p-3 bg-pink-700 rounded-xl">
+            Start asking now
+          </button>
+          <button className=" p-3 ml-2 bg-pink-700 rounded-xl">
+            Watch demo
+          </button>
+        </div>
       </div>
-      <p className="block">fkjekjfh</p>
     </div>
   );
 };
 
 export default App;
-
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,102 @@
 import React from "react";
+import Navbar from "/Advice/frontend/src/components/Navbar";
 
 const Login = () => {
   return (
-    <>
-      <div>Login</div>
-      <div>start</div>
-    </>
+    <div className="bg-gradient-to-br from-[#f0f4ff] via-[#fcece4] to-[#e0f7fa] min-h-screen text-gray-900 flex flex-col">
+      <Navbar />
+
+      <div className="flex justify-center items-center flex-1 px-4">
+        <div className="w-full max-w-5xl bg-white shadow sm:rounded-lg flex flex-col lg:flex-row overflow-hidden">
+          <div className="lg:w-1/2 p-6">
+            <div className="flex flex-col items-center">
+              <h1 className="text-2xl font-extrabold">Sign up</h1>
+              <div className="w-full mt-6 flex flex-col items-center">
+                <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition duration-300 hover:shadow-md">
+                  <div className="bg-white p-2 rounded-full">
+                    <svg className="w-4" viewBox="0 0 533.5 544.3">
+                      <path
+                        d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                        fill="#4285f4"
+                      />
+                      <path
+                        d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                        fill="#34a853"
+                      />
+                      <path
+                        d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                        fill="#fbbc04"
+                      />
+                      <path
+                        d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                        fill="#ea4335"
+                      />
+                    </svg>
+                  </div>
+                  <span className="ml-4">Sign Up with Google</span>
+                </button>
+
+                <button className="mt-4 w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition duration-300 hover:shadow-md">
+                  <div className="bg-white p-1 rounded-full">
+                    <svg className="w-6" viewBox="0 0 32 32">
+                      <path
+                        fillRule="evenodd"
+                        d="M16 4C9.371 4 4 9.371 4 16c0 5.3 3.438 9.8 8.207 11.387.602.11.82-.258.82-.578 0-.286-.011-1.04-.015-2.04-3.34.723-4.043-1.609-4.043-1.609-.547-1.387-1.332-1.758-1.332-1.758-1.09-.742.082-.726.082-.726 1.203.086 1.836 1.234 1.836 1.234 1.07 1.836 2.808 1.305 3.492 1 .11-.777.422-1.305.762-1.605-2.664-.301-5.465-1.332-5.465-5.93 0-1.313.469-2.383 1.234-3.223-.121-.3-.535-1.523.117-3.175 0 0 1.008-.32 3.301 1.23A11.487 11.487 0 0116 9.805c1.02.004 2.047.136 3.004.402 2.293-1.55 3.297-1.23 3.297-1.23.656 1.652.246 2.875.12 3.175.77.84 1.231 1.91 1.231 3.223 0 4.61-2.804 5.621-5.476 5.922.43.367.812 1.101.812 2.219 0 1.605-.011 2.898-.011 3.293 0 .32.214.695.824.578C24.566 25.797 28 21.3 28 16c0-6.629-5.371-12-12-12z"
+                      />
+                    </svg>
+                  </div>
+                  <span className="ml-4">Sign Up with GitHub</span>
+                </button>
+              </div>
+
+              <div className="my-8 border-b text-center">
+                <div className="px-2 text-sm text-gray-600 font-medium bg-white transform translate-y-1/2">
+                  Or sign up with e-mail
+                </div>
+              </div>
+
+              <div className="mx-auto max-w-xs">
+                <input
+                  className="w-full px-6 py-3 rounded-lg bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="email"
+                  placeholder="Email"
+                />
+                <input
+                  className="w-full px-6 py-3 mt-4 rounded-lg bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="password"
+                  placeholder="Password"
+                />
+                <button className="mt-4 bg-indigo-500 text-white w-full py-3 rounded-lg hover:bg-indigo-700 transition duration-300 flex items-center justify-center">
+                  <svg
+                    className="w-5 h-5 -ml-2"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                    <circle cx="8.5" cy="7" r="4" />
+                    <path d="M20 8v6M23 11h-6" />
+                  </svg>
+                  <span className="ml-3">Sign Up</span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="hidden lg:flex flex-1 items-center justify-center">
+            <div
+              className="h-full w-full bg-cover bg-center"
+              style={{
+                backgroundImage:
+                  "url('https://images.unsplash.com/photo-1580894894513-541e068a3e2b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
